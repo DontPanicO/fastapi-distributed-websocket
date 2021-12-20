866c0809074f963e51941bb35750645f3e96a22b
DontPanicO <sviluppo6@metodogroup.it>
2021-12-20 10:17:07 +0100

    Fixed typing

--- distributed_websockets/_connection.py
+++ distributed_websockets/_connection.py
@@ -1,17 +1,17 @@
-from typing import AsyncIterator, Optional, Any
+from typing import AsyncIterator, Optional, Any, Callable, Awaitable, NoReturn
 
 from fastapi import WebSocket, WebSocketDisconnect
 
 
 class Connection:
 
-    def __init__(self, websocket: WebSocket, conn_id: str, topic: Optional[str] = None) -> None:
+    def __init__(self, websocket: WebSocket, conn_id: str, topic: Optional[str] = None) -> NoReturn:
         self.websocket: WebSocket = websocket
         self.id: str = conn_id
         self.topic: Optional[str] = topic
-        self.accept = websocket.accept
-        self.send_json = websocket.send_json
-        self.iter_json = websocket.iter_json
+        self.accept: Callable[[], Awaitable] = websocket.accept
+        self.send_json: Callable[[Any, str], Awaitable] = websocket.send_json
+        self.iter_json: Callable[[], Awaitable] = websocket.iter_json
     
-    async def close(self) -> None:
+    async def close(self) -> NoReturn:
         await self.websocket.close()
