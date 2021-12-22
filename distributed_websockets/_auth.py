63dd83a9e1cbcdd3c001e53661316f6193197013
DontPanicO <sviluppo6@metodogroup.it>
2021-12-22 13:26:04 +0100

    Defined class `WebSocketOAuth2PasswordBearer` inheriting from `OAuth2PasswordBearer` to handle ws authentication

---
+++ distributed_websockets/_auth.py
@@ -0,0 +1,35 @@
+import asyncio
+from typing import Optional, Any, NoReturn
+
+from fastapi import WebSocket, status
+from fastapi.security.utils import get_authorization_scheme_param
+from fastapi.security import OAuth2PasswordBearer
+
+
+class WebSocketOauth2PasswordBearer(OAuth2PasswordBearer):
+    def __init__(
+        self,
+        token_url: str,
+        scheme_name: Optional[str] = None,
+        scopes: Optional[dict[str, str]] = None,
+        description: Optional[str] = None,
+        auto_error: bool = True,
+    ) -> NoReturn:
+        super().__init__(
+            name,
+            token_url,
+            scheme_name,
+            scopes,
+            description,
+            auto_error,
+        )
+
+    async def __call__(self, websocket: WebSocket) -> Optional[str]:
+        authorization: str = websocket.headers.get('Authorization')
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != 'Bearer':
+            if self.auto_error:
+                await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+            else:
+                return None
+        return param
