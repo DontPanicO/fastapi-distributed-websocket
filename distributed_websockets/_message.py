b6ff5d9116df4aafc01222a4b32895b473387ca9
DontPanicO <andry.teddy@gmail.com>
2021-12-24 12:25:36 +0100

    created message tag / untag methods

--- distributed_websockets/_message.py
+++ distributed_websockets/_message.py
@@ -4,6 +4,17 @@ from typing import Optional, Any, NoReturn
 from .utils import is_valid_json
 
 
+def tag_client_message(data: dict, topic: Optional[str] = None) -> Any:
+    if not topic:
+        return data.update({'tpye': 'broadcast'})
+    return data.update({'type': 'publish', 'topic': topic})
+
+
+def untag_broker_message(data: str) -> Any:
+    msg: dict = json.loads(data)
+    return msg.pop('type'), msg.pop('topic'), msg
+
+
 class Message:
     def __init__(
         self,
@@ -15,7 +26,7 @@ class Message:
         self.topic: str = topic
         self.data: Any = data
         self.type: Optional[str] = type
-        self.pattern: Optional[None] = pattern
+        self.pattern: Optional[str] = pattern
 
     @property
     def __dict__(self) -> dict:
@@ -29,6 +40,9 @@ class Message:
     def __serialize__(self) -> str:
         assert is_valid_json(self.data), 'Message data must be valid JSON'
         return json.dumps(self.data)
+
+    def __deserialize__(self) -> Any:
+        return json.loads(self.data)
 
     @classmethod
     def deserialize(cls, data: dict) -> 'Message':
