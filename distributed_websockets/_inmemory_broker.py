72d31e30033304dcc14c8ce45c8e3ecfa90033f5
DontPanicO <andry.teddy@gmail.com>
2021-12-24 12:24:47 +0100

    changed term topic to channel in InMemoryBroker

--- distributed_websockets/_inmemory_broker.py
+++ distributed_websockets/_inmemory_broker.py
@@ -8,19 +8,19 @@ class InMemoryBroker:
         self._subscribers: set = set()
         self._messages: asyncio.Queue = asyncio.Queue()
 
-    async def subscribe(self, topic: str) -> Coroutine[Any, Any, NoReturn]:
-        self._subscribers.add(topic)
+    async def subscribe(self, channel: str) -> Coroutine[Any, Any, NoReturn]:
+        self._subscribers.add(channel)
 
-    async def unsubscribe(self, topic: str) -> Coroutine[Any, Any, NoReturn]:
-        self._subscribers.remove(topic)
+    async def unsubscribe(self, channel: str) -> Coroutine[Any, Any, NoReturn]:
+        self._subscribers.remove(channel)
 
-    async def publish(self, topic: str, message: Any) -> Coroutine[Any, Any, NoReturn]:
-        await self._messages.put({'channel': topic, 'data': message})
+    async def publish(self, channel: str, message: Any) -> Coroutine[Any, Any, NoReturn]:
+        await self._messages.put({'channel': channel, 'data': message})
 
     async def get_message(self) -> Coroutine[Any, Any, Optional[dict]]:
         message = await self._messages.get()
         if message['channel'] in self._subscribers:
             return message
 
-    def has_subscribers(self, topic: str) -> bool:
-        return topic in self._subscribers
+    def has_subscribers(self, channel: str) -> bool:
+        return channel in self._subscribers
