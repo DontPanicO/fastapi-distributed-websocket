9543eb8cf9f654e21c0a20e28d7c73500c5b1a5f
DontPanicO <andry.teddy@gmail.com>
2021-12-24 11:44:22 +0100

    Defined util for developers who need to implement custom brokers to check whether their broker can be used with `WebSocketManager`

--- distributed_websockets/utils.py
+++ distributed_websockets/utils.py
@@ -1,3 +1,4 @@
+import inspect
 from typing import Optional, Any, NoReturn
 
 import asyncio
@@ -22,3 +23,18 @@ def is_valid_json(data: Any) -> bool:
 def serialize(obj: Any) -> Any:
     assert hasattr(obj, '__serialize__'), 'Object must have __serialize__ method'
     return obj.__serialize__()
+
+
+def is_valid_broker(obj: Any) -> bool:
+    """
+    Helper utils to check if an object can
+    be used as a broker in `WebSocketManager`.
+    Exposed to developers who need to implement a
+    custom broker.
+    """
+    return (
+        (hasattr(obj, 'subscribe') and inspect.iscoroutinefunction(obj.subscribe))
+        and (hasattr(obj, 'unsubscribe') and inspect.iscoroutinefunction(obj.unsubscribe))
+        and (hasattr(obj, 'publish') and inspect.iscoroutinefunction(obj.publish))
+        and (hasattr(obj, 'get_message') and inspect.iscoroutinefunction(obj.get_message))
+    )
