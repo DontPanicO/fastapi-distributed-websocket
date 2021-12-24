3637fb53709ae1466b1814a59f33289645baa700
DontPanicO <andry.teddy@gmail.com>
2021-12-24 11:21:44 +0100

    Typing and format

--- distributed_websockets/_connection.py
+++ distributed_websockets/_connection.py
@@ -5,14 +5,19 @@ from fastapi import WebSocket, WebSocketDisconnect, status
 
 
 class Connection:
-
-    def __init__(self, websocket: WebSocket, conn_id: str, topic: Optional[str] = None) -> NoReturn:
+    def __init__(
+        self, websocket: WebSocket, conn_id: str, topic: Optional[str] = None
+    ) -> NoReturn:
         self.websocket: WebSocket = websocket
         self.id: str = conn_id
         self.topic: Optional[str] = topic
-        self.accept: Callable[[], Coroutine[Any, Any, None]] = websocket.accept
-        self.send_json: Callable[[Any, str], Coroutine[Any, Any, None]] = websocket.send_json
+        self.accept: Callable[[], Coroutine[Any, Any, NoReturn]] = websocket.accept
+        self.send_json: Callable[
+            [Any, str], Coroutine[Any, Any, NoReturn]
+        ] = websocket.send_json
         self.iter_json: Callable[[], AsyncIterator] = websocket.iter_json
-    
-    async def close(self, code: int = status.WS_1000_NORMAL_CLOSURE) -> NoReturn:
+
+    async def close(
+        self, code: int = status.WS_1000_NORMAL_CLOSURE
+    ) -> Coroutine[Any, Any, NoReturn]:
         await self.websocket.close(code)
--- distributed_websockets/manager.py
+++ distributed_websockets/manager.py
@@ -1,5 +1,6 @@
 import asyncio
 from typing import Optional, Any, NoReturn
+from collections.abc import Coroutine
 
 import aioredis
 from fastapi import WebSocket, WebSocketDisconnect, status
@@ -16,7 +17,7 @@ class WebSocketManager:
         self.broker: Optional[aioredis.client.PubSub] = None
         self.broker_channel: str = broker_channel
 
-    async def _connect(self, connection: Connection) -> NoReturn:
+    async def _connect(self, connection: Connection) -> Coroutine[Any, Any, NoReturn]:
         await connection.accept()
         self.active_connections.append(connection)
 
@@ -25,15 +26,17 @@ class WebSocketManager:
 
     async def new_connection(
         self, websocket: WebSocket, conn_id: str, topic: Optional[str] = None
-    ) -> Connection:
+    ) -> Coroutine[Any, Any, Connection]:
         connection = Connection(websocket, conn_id, topic)
         await self._connect(connection)
         return connection
 
-    async def remove_connection(self, connection: Connection, code: int = status.WS_1000_NORMAL_CLOSURE) -> NoReturn:
+    async def remove_connection(
+        self, connection: Connection, code: int = status.WS_1000_NORMAL_CLOSURE
+    ) -> Coroutine[Any, Any, NoReturn]:
         await connection.close(code)
         self._disconnect(connection)
-    
+
     def raw_remove_connection(self, connection: Connection) -> NoReturn:
         """
         Use it after a `WebSocketDisconnect` exception.
@@ -42,7 +45,7 @@ class WebSocketManager:
         """
         self._disconnect(connection)
 
-    async def _send(self, topic: str, message: Any) -> NoReturn:
+    async def _send(self, topic: str, message: Any) -> Coroutine[Any, Any, NoReturn]:
         for connection in self.active_connections:
             if connection.topic == topic:
                 await connection.send_json(message)
@@ -50,28 +53,30 @@ class WebSocketManager:
     def send(self, topic: str, message: Any) -> NoReturn:
         self._send_tasks.append(asyncio.create_task(self._send(topic, message)))
 
-    async def _to_broker(self, message: Any) -> NoReturn:
+    async def _to_broker(self, message: Any) -> Coroutine[Any, Any, NoReturn]:
         await self.broker.publish(self.broker_channel, message)
-    
-    async def receive(self, message: Any) -> NoReturn:
+
+    async def receive(self, message: Any) -> Coroutine[Any, Any, NoReturn]:
         pass
 
-    async def _from_broker(self) -> NoReturn:
+    async def _from_broker(self) -> Coroutine[Any, Any, NoReturn]:
         while True:
             message = await self.broker.get_message(ignore_subscribe_messages=True)
             self.send(message['channel'], message['data'])
 
-    async def broadcast(self, message: Any) -> NoReturn:
+    async def broadcast(self, message: Any) -> Coroutine[Any, Any, NoReturn]:
         for connection in self.active_connections:
             await connection.send_json(message)
-    
-    async def startup(self) -> NoReturn:
+
+    async def startup(self) -> Coroutine[Any, Any, NoReturn]:
         await self.broker.subscribe(self.broker_channel)
         self._main_task = asyncio.create_task(self._from_broker())
 
-    async def shutdown(self) -> NoReturn:
+    async def shutdown(self) -> Coroutine[Any, Any, NoReturn]:
         for task in self._send_tasks:
             clear_task(task)
         for connection in self.active_connections:
-            await self.remove_connection(connection, code=status.WS_1012_SERVICE_RESTART)
+            await self.remove_connection(
+                connection, code=status.WS_1012_SERVICE_RESTART
+            )
         clear_task(self._main_task)
