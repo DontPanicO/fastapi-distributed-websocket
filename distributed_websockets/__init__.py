b1638e2cd2a6398785713cf24557530c2f7f8234
DontPanicO <sviluppo6@metodogroup.it>
2022-01-05 08:54:27 +0100

    init file

--- distributed_websockets/__init__.py
+++ distributed_websockets/__init__.py
@@ -1,3 +1,10 @@
 from ._auth import WebSocketOAuth2PasswordBearer
 from ._connection import Connection
+from ._inmemory_broker import InMemoryBroker
 from .manager import WebSocketManager
+from .types import BrokerT
+from .utils import is_valid_broker
+
+
+__version___ = '0.1.0'
+__author__ = 'DontPanicO'
