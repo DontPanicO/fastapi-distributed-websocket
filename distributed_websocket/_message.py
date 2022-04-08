8285cb15217d03a82a1667f34f765e453f3b81f0
DontPanicO <andry.teddy@gmail.com>
2022-04-08 11:19:53 +0200

    Changed `tag_client_message` type from publish to send to avoid confusion. This also open for a cleaner definition of the `WebSocketManager.send_msg` method

--- distributed_websocket/_message.py
+++ distributed_websocket/_message.py
@@ -7,7 +7,7 @@ from .utils import update
 def tag_client_message(data: dict, topic: str | None = None) -> Any:
     if not topic:
         return update(data, **{'type': 'broadcast', 'topic': None})
-    return update(data, **{'type': 'publish', 'topic': topic})
+    return update(data, **{'type': 'send', 'topic': topic})
 
 
 def untag_broker_message(data: dict | str) -> Any:
