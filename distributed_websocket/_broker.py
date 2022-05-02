3204bb8771799525d5364c87dc71360f1695bb09
Andrea Tedeschi <sviluppo6@metodogroup.it>
2022-05-02 18:02:35 +0200

    redefined `RedisBroker` to be tested

--- distributed_websocket/_broker.py
+++ distributed_websocket/_broker.py
@@ -7,6 +7,8 @@ from urllib.parse import urlparse
 from aioredis import Redis
 from aioredis.client import PubSub
 
+from ._message import Message, untag_broker_message
+
 
 class BrokerInterface(ABC):
     @abstractmethod
@@ -24,7 +26,7 @@ class BrokerInterface(ABC):
         pass
 
     @abstractmethod
-    async def get_message(self, **kwargs) -> Coroutine[Any, Any, dict | None]:
+    async def get_message(self, **kwargs) -> Coroutine[Any, Any, Message | None]:
         pass
 
 
@@ -52,22 +54,42 @@ class InMemoryBroker(BrokerInterface):
     ) -> Coroutine[Any, Any, NoReturn]:
         await self._messages.put({'channel': channel, 'data': message})
 
-    async def get_message(self, **kwargs) -> Coroutine[Any, Any, dict | None]:
+    async def get_message(self, **kwargs) -> Coroutine[Any, Any, Message | None]:
         message = await self._messages.get()
-        if message['channel'] in self._subscribers:
-            return message
+        if self.has_subscribers(message['channel'])
+            typ, topic, data = untag_broker_message(message['data'])
+            return Message(typ=typ, topic=topic, data=data)
 
     def has_subscribers(self, channel: str) -> bool:
         return channel in self._subscribers
 
 
-class RedisBroker(PubSub, BrokerInterface):
+class RedisBroker(BrokerInterface):
+    def __init__(self, redis_url: str,) -> NoReturn:
+        self._redis: Redis = Redis.from_url(redis_url)
+        self._pubsub: PubSub = self._redis.pubsub()
+    
+    async def __aenter__(self) -> Coroutine[Any, Any, BrokerInterface]:
+        return self
+    
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> NoReturn:
+        await self._pubsub.reset()
+        await self._redis.close()
 
-    '''
-    This class does not override any of `aioredis.client.PubSub` methods,
-    while also inheriting from `BrokerInterface` to have logical cohesion
-    with broker objects
-    '''
+    async def subscribe(self, channel: str) -> Coroutine[Any, Any, NoReturn]:
+        await self._pubsub.subscribe(channel)
+    
+    async def unsubscribe(self, channel: str) -> Coroutine[Any, Any, NoReturn]:
+        await self._pubsub.unsubscribe(channel)
+    
+    async def publish(self, channel: str, message: Any) -> Coroutine[Any, Any, NoReturn]:
+        await self._redis.publish(channel, message)
+    
+    async def get_message(self, **kwargs) -> Coroutine[Any, Any, Message | None]:
+        message = await self._pubsub.get_message(ignore_subscribe_messages=True)
+        if message:
+            typ, topic, data = untag_broker_message(message['data'])
+            return Message(typ=typ, topic=topic, data=data)
 
 
 def _create_inmemory_broker() -> InMemoryBroker:
@@ -76,17 +98,14 @@ def _create_inmemory_broker() -> InMemoryBroker:
 
 def _create_redis_broker(
     redis_url: str,
-    shard_hint: str | None = None,
-    ignore_subscribe_messages: bool = False,
 ) -> RedisBroker:
-    redis = Redis.from_url(redis_url)
-    return RedisBroker(redis.connection_pool, shard_hint, ignore_subscribe_messages)
+    return RedisBroker(redis_url)
 
 
 def create_broker(broker_url: str, **kwargs) -> BrokerInterface:
     url = urlparse(broker_url)
     if url.scheme == 'redis':
-        return _create_redis_broker(url.netloc, **kwargs)
+        return _create_redis_broker(url.netloc)
     elif url.scheme == 'memory':
         return _create_inmemory_broker()
     raise ValueError(f'Unknown broker url: {broker_url}')
