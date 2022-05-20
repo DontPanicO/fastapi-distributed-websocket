9c350a01ff27f702780be26b9500a8f10a074883
Andrea Tedeschi <sviluppo6@metodogroup.it>
2022-05-20 23:47:55 +0200

    created subscriptions module

---
+++ distributed_websocket/_subscriptions.py
@@ -0,0 +1,26 @@
+from typing import Any, NoReturn
+
+from ._connection import Connection
+from ._message import Message
+
+
+def _is_subscription_message(message: Message) -> bool:
+    return message.typ == 'subscribe' or message.typ == 'unsubscribe'
+
+
+def _is_valid_subscription(topic: str) -> bool:
+    return topic is not None and len(topic) and '/' in topic
+
+
+def _check_subscription_message(message: Message) -> bool:
+    return _is_subscription_message(message) and _is_valid_subscription(message.topic)
+
+
+def subscribe(connection: Connection, message: Message) -> NoReturn:
+    assert _check_subscription_message(message), f'Message {message} is not valid for sub/unsub'
+    connection.topics.add(message.topic)
+
+def unsubscribe(connection: Connection, message: Message) -> NoReturn:
+    assert _check_subscription_message(message), f'Message {message} is not valid for sub/unsub'
+    if topic in connection.topics:
+        connection.topics.remove(message.topic)
