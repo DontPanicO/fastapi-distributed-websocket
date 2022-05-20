6279c8de23c2295628d970a755a41f5e6468555c
Andrea Tedeschi <sviluppo6@metodogroup.it>
2022-05-21 00:02:45 +0200

    black formatting

--- distributed_websocket/_matching.py
+++ distributed_websocket/_matching.py
@@ -5,10 +5,10 @@ from collections.abc import Generator
 def _match_topic_with_wildcards(topic: str, pattern: str) -> bool:
     return (
         topic == pattern
-        or '#' == pattern[:1]
-        or pattern[:1] in (topic[:1], '+')
+        or "#" == pattern[:1]
+        or pattern[:1] in (topic[:1], "+")
         and _match_topic_with_wildcards(
-            topic[1:], pattern['+' != pattern[:1] or (topic[:1] in '/') * 2 :]
+            topic[1:], pattern["+" != pattern[:1] or (topic[:1] in "/") * 2 :]
         )
     )
 
@@ -18,4 +18,3 @@ def matches(topic: str, patterns: set) -> bool:
         if _match_topic_with_wildcards(topic, pattern):
             return True
     return False
-    
