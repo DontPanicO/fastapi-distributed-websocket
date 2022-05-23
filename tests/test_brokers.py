1fd804a51edbdbcf26b466801fbfaa6ac9ca9bb4
DontPanicO <andry.teddy@gmail.com>
2022-05-23 13:34:59 +0200

    tests

---
+++ tests/test_brokers.py
@@ -0,0 +1,30 @@
+import asyncio
+
+import pytest
+from distributed_websocket._broker import create_broker
+from distributed_websocket._message import tag_client_message
+
+
+@pytest.mark.asyncio
+async def test_inmemory_broker() -> None:
+    async with create_broker('memory://') as broker:
+        await broker.subscribe('test')
+        await broker.publish('test', tag_client_message({'msg': 'hello'}))
+        message = await broker.get_message()
+        assert message.data == {'msg': 'hello'}
+
+
+@pytest.mark.asyncio
+async def test_redis_broker() -> None:
+    async with create_broker('redis://redis') as broker:
+        await broker.subscribe('test')
+        await broker.publish('test', tag_client_message({'msg': 'hello'}))
+        i = 0
+        message = await broker.get_message()
+        while message is None:
+            await asyncio.sleep(0.1)
+            message = await broker.get_message()
+            if i > 10:
+                break
+            i += 1
+        assert message.data == {'msg': 'hello'}
