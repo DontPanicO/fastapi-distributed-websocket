ed8e74990c7282ce2d087918884d247501ae9f60
DontPanicO <andry.teddy@gmail.com>
2022-05-23 09:30:22 +0200

    created decorators to handle specific exceptions in functions (READ THE COMMENT)

---
+++ distributed_websocket/_decorators.py
@@ -0,0 +1,40 @@
+import inspect
+from typing import Any, NoReturn
+from collections.abc import Coroutine, Callable
+from functools import wraps
+
+
+async def _await_maybe(result: Any) -> Any:
+    if inspect.isawaitable(result):
+        return await result
+    return result
+
+
+def handle(exc: BaseException, handler: Callable[..., Any]) -> Callable[..., Any]:
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                return func(*args, **kwargs)
+            except exc as e:
+                return handler(e)
+
+        return wrapper
+
+    return decorator
+
+
+def ahandle(
+    exc: BaseException, handler: Callable[..., Coroutine[Any, Any, Any]]
+) -> Callable[..., Any]:
+    def decorator(func: Callable[..., Any]) -> Callable[..., Coroutine[Any, Any, Any]]:
+        @wraps(func)
+        async def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                return await _await_maybe(func(*args, **kwargs))
+            except exc as e:
+                return await handler(e)
+
+        return wrapper
+
+    return decorator
