02f64de4a38b4db551f8eff1332e414b9206bf04
DontPanicO <andry.teddy@gmail.com>
2022-06-01 10:09:30 +0200

    defined message type validation

--- distributed_websocket/_message.py
+++ distributed_websocket/_message.py
@@ -4,12 +4,32 @@ from typing import Any, NoReturn
 from .utils import update
 
 
+__VALID_TYPES = {
+    'connect',
+    'set_conn_id',
+    'subscribe',
+    'unsubscribe',
+    'broadcast',
+    'send',
+    'send_by_conn_id',
+}
+__SERVER_ONLY_TYPES = {'set_conn_id'}
+__CLIENT_ONLY_TYPES = {'connect'}
+
+
+def is_valid_type_message(data: dict) -> bool:
+    return data.get('type') in __VALID_TYPES
+
+
+def is_valid_type_client_message(data: dict) -> bool:
+    return data.get('type') in __VALID_TYPES - __SERVER_ONLY_TYPES
+
+
 def tag_client_message(data: dict) -> Any:
     topic = data.get('topic', None)
     if topic is None:
         return update(data, **{'type': 'broadcast'})
     return data
-    
 
 
 def untag_broker_message(data: dict | str) -> Any:
@@ -23,11 +43,11 @@ class Message:
         self.typ = typ
         self.topic = topic
         self.data = data
-    
+
     @classmethod
     def from_client_message(cls, *, data: Any) -> 'Message':
         return cls(data=data, typ=data['type'], topic=data['topic'])
-    
+
     def __serialize__(self) -> dict[str, Any]:
         self.data['type'] = self.typ
         self.data['topic'] = self.topic
