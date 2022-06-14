6ddfdfbbc531b70ef9be52b3eebe8b6197f44327
Andrea Tedeschi <sviluppo6@metodogroup.it>
2022-06-14 20:56:06 +0200

    websocket proxy

---
+++ distributed_websocket/proxy.py
@@ -0,0 +1,34 @@
+__all__ = []
+
+import asyncio
+from typing import Any, NoReturn
+from collections.abc import Coroutine
+
+import websockets
+from fastapi import WebSocket, WebSocketDisconnect
+
+
+async def _forward(
+    client: WebSocket, target: websockets.WebSocketClientProtocol
+) -> Coroutine[Any, Any, NoReturn]:
+    async for message in client.iter_text():
+        await target.send(message)
+
+
+async def _reverse(
+    client: WebSocket, target: websockets.WebSocketClientProtocol
+) -> Coroutine[Any, Any, NoReturn]:
+    async for message in target:
+        await client.send_text(message)
+
+
+class WebSocketProxy:
+    def __init__(self, client: WebSocket, server_endpoint: str) -> NoReturn:
+        self._client = client
+        self._server_endpoint = server_endpoint
+        self._tasks: set[asyncio.Task] = set()
+
+    async def __call__(self) -> Coroutine[Any, Any, NoReturn]:
+        async with websockets.connection(self._server_endpoint) as server:
+            self._tasks.add(asyncio.create_task(_forward(self._client, server)))
+            self._tasks.add(asyncio.create_task(_reverse(self._client, server)))
