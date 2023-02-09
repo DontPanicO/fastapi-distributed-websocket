c8b8a5f90aa0d65ec1f2a6fd375384a89e269bb9
DontPanicO <dontpanic.call0@gmail.com>
2023-02-09 13:35:44 +0100

    test for message

---
+++ tests/test_message.py
@@ -0,0 +1,45 @@
+import pytest
+
+from distributed_websocket._message import Message, untag_broker_message
+
+
+def test_message_01():
+    m = Message(
+        data={'msg': 'hello'},
+        typ='send',
+        topic='test',
+        conn_id='test',
+    )
+    assert m.data == {'msg': 'hello'}
+    data = m.__serialize__()
+    assert data == {
+        'msg': 'hello',
+        'type': 'send',
+        'topic': 'test',
+        'conn_id': 'test',
+    }
+
+
+def test_message_02():
+    m = Message.from_client_message(
+        data={'msg': 'hello', 'type': 'send', 'topic': 'test', 'conn_id': 'test'}
+    )
+    assert m.data == {
+        'msg': 'hello',
+        'type': 'send',
+        'topic': 'test',
+        'conn_id': 'test',
+    }
+    assert m.typ == 'send'
+    assert m.topic == 'test'
+    assert m.conn_id == 'test'
+
+
+def test_untag_broker_message_01():
+    typ, topic, conn_id, data = untag_broker_message(
+        '{"msg": "hello", "type": "send", "topic": "test", "conn_id": "test"}'
+    )
+    assert typ == 'send'
+    assert topic == 'test'
+    assert conn_id == 'test'
+    assert data == {'msg': 'hello'}
