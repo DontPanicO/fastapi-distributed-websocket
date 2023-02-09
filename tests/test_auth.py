2a45d67a12a97f9bfd3aa1e68760bbdd45f2a11e
DontPanicO <dontpanic.call0@gmail.com>
2023-02-09 08:55:52 +0100

    annotated `test_client_factory` in tests and new test for auth

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,24 +1,26 @@
 from collections.abc import Callable
 
-import pytest
 from starlette import status
 from starlette.types import Receive, Scope, Send
 from starlette.testclient import TestClient
 from starlette.websockets import WebSocket, WebSocketDisconnect, WebSocketState
 
-from distributed_websocket._connection import Connection
+from distributed_websocket._auth import WebSocketOAuth2PasswordBearer
 
 
-def test_connection_send(
+def test_oauth2_password_bearer(
     test_client_factory: Callable[[Callable[[Scope, Receive, Send], None]], TestClient]
-) -> None:
+):
     async def app(scope: Scope, receive: Receive, send: Send) -> None:
         websocket = WebSocket(scope, receive=receive, send=send)
         await websocket.accept()
-        connection = Connection(websocket, 'test')
-        await connection.send_json({'your_id': connection.id})
+        auth = WebSocketOAuth2PasswordBearer('http://testserver/v1/access_token/')
+        token = await auth(websocket)
+        await websocket.send_json({'your_id': token})
 
     client = test_client_factory(app)
-    with client.websocket_connect('/') as websocket:
+    with client.websocket_connect(
+        '/', headers={'Authorization': 'Bearer test'}
+    ) as websocket:
         data = websocket.receive_json()
         assert data == {'your_id': 'test'}
