81702e338f48cf13a3d2ff0d52b6938e76735197
DontPanicO <dontpanic.call0@gmail.com>
2023-02-09 09:03:50 +0100

    auth missing header test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable
 
+import pytest
 from starlette import status
 from starlette.types import Receive, Scope, Send
 from starlette.testclient import TestClient
@@ -24,3 +25,20 @@ def test_oauth2_password_bearer(
     ) as websocket:
         data = websocket.receive_json()
         assert data == {'your_id': 'test'}
+
+
+def test_oauth2_password_bearer_no_auth(
+    test_client_factory: Callable[[Callable[[Scope, Receive, Send], None]], TestClient]
+):
+    async def app(scope: Scope, receive: Receive, send: Send) -> None:
+        websocket = WebSocket(scope, receive=receive, send=send)
+        await websocket.accept()
+        auth = WebSocketOAuth2PasswordBearer('http://testserver/v1/access_token/')
+        token = await auth(websocket)
+        with pytest.raises(RuntimeError):
+            await websocket.send_json({'your_id': token})
+
+    client = test_client_factory(app)
+    with client.websocket_connect('/') as websocket:
+        with pytest.raises(WebSocketDisconnect):
+            websocket.receive_json()
