2eef8f30ccf5e0e4b0faaeb5beecb77aef72d69e
DontPanicO <dontpanic.call0@gmail.com>
2023-02-11 13:45:29 +0000

    test for ws manager

---
+++ tests/test_manager.py
@@ -0,0 +1,299 @@
+import asyncio
+import signal
+from collections.abc import Callable
+
+import pytest
+from starlette import status
+from starlette.types import Receive, Scope, Send
+from starlette.testclient import TestClient
+from starlette.websockets import WebSocket, WebSocketDisconnect, WebSocketState
+
+from distributed_websocket import WebSocketManager, Connection, Message
+
+
+def test_manager_01(
+    test_client_factory: Callable[
+        [Callable[[Scope, Receive, Send], None]], TestClient
+    ]
+):
+    manager = WebSocketManager('test', 'memory://')
+
+    async def app(scope: Scope, receive: Receive, send: Send) -> None:
+        websocket = WebSocket(scope, receive, send)
+        connection = await manager.new_connection(websocket, 'conn1')
+        connection.topics.add('tests/1')
+        manager.send(
+            Message(
+                data={'msg': 'hello'},
+                typ='send',
+                topic='tests/1',
+            )
+        )
+
+    test_client = test_client_factory(app)
+    with test_client.websocket_connect('/') as websocket:
+        msg = websocket.receive_json()
+        assert msg == {'msg': 'hello'}
+
+    for t in manager._send_tasks:
+        if not t.done():
+            t.cancel()
+
+
+def test_manager_02(
+    test_client_factory: Callable[
+        [Callable[[Scope, Receive, Send], None]], TestClient
+    ]
+):
+    manager = WebSocketManager('test', 'memory://')
+
+    async def app(scope: Scope, receive: Receive, send: Send) -> None:
+        websocket = WebSocket(scope, receive, send)
+        conn_id = websocket.query_params['myid'] or 'connx'
+        connection = await manager.new_connection(websocket, conn_id)
+        connection.topics.add('tests/1')
+        manager.send(
+            Message(
+                data={'msg': 'hello'},
+                typ='send',
+                topic='tests/1',
+            )
+        )
+
+    test_client1 = test_client_factory(app)
+    test_client2 = test_client_factory(app)
+    with test_client1.websocket_connect(
+        '/?myid=conn1'
+    ) as w1, test_client2.websocket_connect('/?myid=conn2') as w2:
+        msg1 = w1.receive_json()
+        msg2 = w2.receive_json()
+        assert msg1 == msg2 == {'msg': 'hello'}
+
+    for t in manager._send_tasks:
+        if not t.done():
+            t.cancel()
+
+
+@pytest.mark.asyncio
+async def test_manager_broadcast(
+    test_client_factory: Callable[
+        [Callable[[Scope, Receive, Send], None]], TestClient
+    ],
+):
+    manager = WebSocketManager('test', 'memory://')
+    await manager.startup()
+
+    async def app(scope: Scope, receive: Receive, send: Send) -> None:
+        websocket = WebSocket(scope, receive, send)
+        conn_id = websocket.query_params['myid'] or 'connx'
+        connection = await manager.new_connection(websocket, conn_id)
+        connection.topics.add('tests/1')
+        async for m in connection.iter_json():
+            await manager.receive(connection, m)
+        manager.remove_connection(connection)
+
+    test_client1 = test_client_factory(app)
+    test_client2 = test_client_factory(app)
+    with test_client1.websocket_connect(
+        '/?myid=conn1'
+    ) as w1, test_client2.websocket_connect('/?myid=conn2') as w2:
+        w1.send_json(
+            {
+                'type': 'broadcast',
+                'topic': 'tests/1',
+                'conn_id': 'conn1',
+                'msg': 'hello',
+            }
+        )
+        await asyncio.sleep(0.01)
+        msg1 = w1.receive_json()
+        msg2 = w2.receive_json()
+        assert msg1 == msg2 == {'msg': 'hello'}
+
+    await manager.shutdown()
+
+
+@pytest.mark.asyncio
+async def test_manager_send_01(
+    test_client_factory: Callable[
+        [Callable[[Scope, Receive, Send], None]], TestClient
+    ],
+):
+    manager = WebSocketManager('test', 'memory://')
+    await manager.startup()
+
+    async def app(scope: Scope, receive: Receive, send: Send) -> None:
+        websocket = WebSocket(scope, receive, send)
+        conn_id = websocket.query_params['myid'] or 'connx'
+        connection = await manager.new_connection(websocket, conn_id)
+        connection.topics.add('tests/1')
+        async for m in connection.iter_json():
+            await manager.receive(connection, m)
+        manager.remove_connection(connection)
+
+    test_client1 = test_client_factory(app)
+    test_client2 = test_client_factory(app)
+    with test_client1.websocket_connect(
+        '/?myid=conn1'
+    ) as w1, test_client2.websocket_connect('/?myid=conn2') as w2:
+        w1.send_json(
+            {
+                'type': 'send',
+                'topic': 'tests/1',
+                'conn_id': 'conn1',
+                'msg': 'hello',
+            }
+        )
+        await asyncio.sleep(0.01)
+        msg1 = w1.receive_json()
+        msg2 = w2.receive_json()
+        assert msg1 == msg2 == {'msg': 'hello'}
+
+    await manager.shutdown()
+
+
+@pytest.mark.asyncio
+async def test_manager_send_02(
+    test_client_factory: Callable[
+        [Callable[[Scope, Receive, Send], None]], TestClient
+    ],
+):
+    manager = WebSocketManager('test', 'memory://')
+    await manager.startup()
+
+    async def app(scope: Scope, receive: Receive, send: Send) -> None:
+        websocket = WebSocket(scope, receive, send)
+        conn_id = websocket.query_params['myid'] or 'connx'
+        connection = await manager.new_connection(websocket, conn_id)
+        connection.topics.add('tests/1')
+        async for m in connection.iter_json():
+            await manager.receive(connection, m)
+        manager.remove_connection(connection)
+
+    test_client1 = test_client_factory(app)
+    test_client2 = test_client_factory(app)
+    with test_client1.websocket_connect(
+        '/?myid=conn1'
+    ) as w1, test_client2.websocket_connect('/?myid=conn2') as w2:
+        w1.send_json(
+            {
+                'type': 'send',
+                'topic': 'tests/2',
+                'conn_id': 'conn1',
+                'msg': 'hello',
+            }
+        )
+        await asyncio.sleep(0.01)
+        msg1 = msg2 = None
+
+        def get_msg1():
+            nonlocal msg1
+            msg1 = w1.receive_json()
+
+        def get_msg2():
+            nonlocal msg2
+            msg2 = w2.receive_json()
+
+        signal.signal(signal.SIGALRM, get_msg1)
+        signal.signal(signal.SIGALRM, get_msg2)
+        signal.alarm(1)
+        assert msg1 == msg2 == None
+
+    await manager.shutdown()
+
+
+@pytest.mark.asyncio
+async def test_manager_send_by_conn_id_01(
+    test_client_factory: Callable[
+        [Callable[[Scope, Receive, Send], None]], TestClient
+    ],
+):
+    manager = WebSocketManager('test', 'memory://')
+    await manager.startup()
+
+    async def app(scope: Scope, receive: Receive, send: Send) -> None:
+        websocket = WebSocket(scope, receive, send)
+        conn_id = websocket.query_params['myid'] or 'connx'
+        connection = await manager.new_connection(websocket, conn_id)
+        connection.topics.add('tests/1')
+        async for m in connection.iter_json():
+            await manager.receive(connection, m)
+        manager.remove_connection(connection)
+
+    test_client1 = test_client_factory(app)
+    test_client2 = test_client_factory(app)
+    with test_client1.websocket_connect(
+        '/?myid=conn1'
+    ) as w1, test_client2.websocket_connect('/?myid=conn2') as w2:
+        w1.send_json(
+            {
+                'type': 'send_by_conn_id',
+                'topic': None,
+                'conn_id': 'conn3',
+                'msg': 'hello',
+            }
+        )
+        await asyncio.sleep(0.01)
+        msg1 = msg2 = None
+
+        def get_msg1():
+            nonlocal msg1
+            msg1 = w1.receive_json()
+
+        def get_msg2():
+            nonlocal msg2
+            msg2 = w2.receive_json()
+
+        signal.signal(signal.SIGALRM, get_msg1)
+        signal.signal(signal.SIGALRM, get_msg2)
+        signal.alarm(1)
+        assert msg1 == msg2 == None
+
+    await manager.shutdown()
+
+
+@pytest.mark.asyncio
+async def test_manager_send_by_conn_id_02(
+    test_client_factory: Callable[
+        [Callable[[Scope, Receive, Send], None]], TestClient
+    ],
+):
+    manager = WebSocketManager('test', 'memory://')
+    await manager.startup()
+
+    async def app(scope: Scope, receive: Receive, send: Send) -> None:
+        websocket = WebSocket(scope, receive, send)
+        conn_id = websocket.query_params['myid'] or 'connx'
+        connection = await manager.new_connection(websocket, conn_id)
+        connection.topics.add('tests/1')
+        async for m in connection.iter_json():
+            await manager.receive(connection, m)
+        manager.remove_connection(connection)
+
+    test_client1 = test_client_factory(app)
+    test_client2 = test_client_factory(app)
+    with test_client1.websocket_connect(
+        '/?myid=conn1'
+    ) as w1, test_client2.websocket_connect('/?myid=conn2') as w2:
+        w1.send_json(
+            {
+                'type': 'send_by_conn_id',
+                'topic': None,
+                'conn_id': 'conn2',
+                'msg': 'hello',
+            }
+        )
+        await asyncio.sleep(0.01)
+        msg1 = None
+
+        def get_msg1():
+            nonlocal msg1
+            msg2 = w1.receive_json()
+
+        signal.signal(signal.SIGALRM, get_msg1)
+        msg2 = w2.receive_json()
+        signal.alarm(1)
+        assert msg1 is None
+        assert msg2 == {'msg': 'hello'}
+
+    await manager.shutdown()
